c30dee0c6c5596d9782e72170f6f5a6cabe67268
Nikolay <nikolay.dubina.pub@gmail.com>
2021-01-10 17:30:26 +0800

    made count vectorizer stable for gofuzz. makefile

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ test:
 bench:
 	go test -bench=. -benchtime=3s -benchmem ./...
 
+bench-transform:
+	go test -bench=_Transform_ -benchtime=3s -benchmem ./...
+
 profile:
 	$(shell mkdir -p benchmark_profiles)
 	go test -bench=BenchmarkWith32FieldsFeatureTransformer_Transform -benchtime=3s -benchmem -memprofile benchmark_profiles/codegen_transform_mem.profile -cpuprofile benchmark_profiles/codegen_transform_cpu.profile ./cmd/generate/tests
--- transformers/textprocessors_test.go
+++ transformers/textprocessors_test.go
@@ -11,13 +11,13 @@ func TestCountVectorizerFit(t *testing.T) {
 	samples := []struct {
 		name   string
 		input  []string
-		output map[string]int
+		output map[string]uint
 	}{
-		{"basic", []string{"a b", "b a", "a", "b", ""}, map[string]int{"a": 0, "b": 1}},
-		{"same_string", []string{"a", "a", "a"}, map[string]int{"a": 0}},
-		{"empty_string", []string{"", "", ""}, map[string]int{}},
-		{"zeros_single", []string{""}, map[string]int{}},
-		{"single", []string{"a"}, map[string]int{"a": 0}},
+		{"basic", []string{"a b", "b a", "a", "b", ""}, map[string]uint{"a": 0, "b": 1}},
+		{"same_string", []string{"a", "a", "a"}, map[string]uint{"a": 0}},
+		{"empty_string", []string{"", "", ""}, map[string]uint{}},
+		{"zeros_single", []string{""}, map[string]uint{}},
+		{"single", []string{"a"}, map[string]uint{"a": 0}},
 		{"empty", nil, nil},
 	}
 
@@ -44,13 +44,13 @@ func TestTFIDFVectorizerFit(t *testing.T) {
 	samples := []struct {
 		name        string
 		ndocs       int
-		doccount    map[int]int
-		mapping     map[string]int
+		doccount    map[int]uint
+		mapping     map[string]uint
 		input       []string
 		numFeatures int
 	}{
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, []string{"a a a b b", "a a a c", "a a", "a a a", "a a a a", "a a a c c"}, 3},
-		{"empty encoder empty input", 0, map[int]int(nil), map[string]int(nil), nil, 0},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, []string{"a a a b b", "a a a c", "a a", "a a a", "a a a a", "a a a c c"}, 3},
+		{"empty encoder empty input", 0, map[int]uint(nil), map[string]uint(nil), nil, 0},
 	}
 
 	for _, s := range samples {
@@ -79,21 +79,21 @@ func TestTFIDFVectorizerTransform(t *testing.T) {
 	samples := []struct {
 		name     string
 		ndocs    int
-		doccount map[int]int
-		mapping  map[string]int
+		doccount map[int]uint
+		mapping  map[string]uint
 		input    string
 		output   []float64
 	}{
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "a a a c", []float64{0.8194099510753755, 0, 0.5732079309279058}},
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "a a", []float64{1, 0, 0}},
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "a a a", []float64{1, 0, 0}},
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "a a a a", []float64{1, 0, 0}},
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "a a a b b", []float64{0.47330339145578754, 0.8808994832762984, 0}},
-		{"basic", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "a a a c c", []float64{0.58149260706886, 0, 0.8135516873095773}},
-		{"not found", 6, map[int]int{0: 6, 1: 1, 2: 2}, map[string]int{"a": 0, "b": 1, "c": 2}, "dddd", []float64{0, 0, 0}},
-		{"empty input", 2, map[int]int{0: 1, 1: 2}, map[string]int{"a": 0, "b": 1}, "     ", []float64{0, 0}},
-		{"empty vals", 2, map[int]int{0: 1, 1: 2}, map[string]int{}, " b  a  ", []float64{}},
-		{"nil input", 2, map[int]int{0: 1, 1: 2}, map[string]int{}, "", []float64{}},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "a a a c", []float64{0.8194099510753755, 0, 0.5732079309279058}},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "a a", []float64{1, 0, 0}},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "a a a", []float64{1, 0, 0}},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "a a a a", []float64{1, 0, 0}},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "a a a b b", []float64{0.47330339145578754, 0.8808994832762984, 0}},
+		{"basic", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "a a a c c", []float64{0.58149260706886, 0, 0.8135516873095773}},
+		{"not found", 6, map[int]uint{0: 6, 1: 1, 2: 2}, map[string]uint{"a": 0, "b": 1, "c": 2}, "dddd", []float64{0, 0, 0}},
+		{"empty input", 2, map[int]uint{0: 1, 1: 2}, map[string]uint{"a": 0, "b": 1}, "     ", []float64{0, 0}},
+		{"empty vals", 2, map[int]uint{0: 1, 1: 2}, map[string]uint{}, " b  a  ", []float64{}},
+		{"nil input", 2, map[int]uint{0: 1, 1: 2}, map[string]uint{}, "", []float64{}},
 	}
 
 	for _, s := range samples {
